812334a788697157d2c0a038c6f22933cf708858
Antoine Gualy <gualyantoine@example.com>
2025-12-17 00:12:52 -0700

    Set up Money-Pot MVP files

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from flask import Flask
+# Imports 
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+
+# My App
+app = Flask(__name__)
+
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template('index.html')
+
+
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
